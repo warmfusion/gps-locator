fd88c60b072225853d49e692d1fcef8d292447ff
Toby Jackson <admin@warmfusion.co.uk>
2014-05-13 21:42:08 +0100

    Initial test implementation of haversine function to determine distance

--- toby_gps_lowpower.ino
+++ toby_gps_lowpower.ino
@@ -3,7 +3,7 @@
 int led_pin = 13;
 ISR(WDT_vect) { Sleepy::watchdogEvent(); } // Setup the watchdog
 TinyGPS gps; //initialise GPS object
-float tobyLat=55.582913, tobyLon=-1.835315;
+float targetLat=55.582913, targetLon=-1.835315;
 float nearDist = 0.0002, farDist = 0.001;
 int nearPin = 5, farPin = 6, lostPin = 7, powerPin = 2;
 int lostCount=0;
@@ -78,16 +78,19 @@ void lost()
 void notLost()
 {
     lostCount=0;
-    float flat,flon;
+    float flat,flon,dist;
     unsigned long age;
     gps.f_get_position(&flat, &flon, &age);    
-    if (((tobyLat-nearDist)<=flat && flat<=(tobyLat+nearDist)) && ((tobyLon-nearDist)<=flon && flon<=(tobyLon+nearDist)))
+
+    dist = calc_dist( flat, flon, targetLat, targetLon);
+
+    if (dist < nearDist)
     {
       digitalWrite(nearPin,HIGH);
       digitalWrite(farPin,LOW);
       digitalWrite(lostPin,LOW);      
     }
-    else if (((tobyLat-farDist)<flat && flat<(tobyLat+farDist)) && ((tobyLon-farDist)<flon && flon<(tobyLon+farDist)))
+    else if (dist < farDist)
     {
       digitalWrite(nearPin,LOW);
       digitalWrite(farPin,HIGH);
@@ -103,3 +106,39 @@ void notLost()
     Sleepy::loseSomeTime(10000);
 }
   
+
+
+/*************************************************************************
+ * //Function to calculate the distance between two waypoints
+ * //From: http://forum.arduino.cc/index.php/topic,27541.0.html
+ *
+ * // Appears to be based on the haversine formular
+ *      - http://blog.avangardo.com/2013/02/distance-between-two-points-on-the-earth/
+ *************************************************************************/
+float calc_dist(float flat1, float flon1, float flat2, float flon2)
+{
+    float dist_calc=0;
+    float dist_calc2=0;
+    float diflat=0;
+    float diflon=0;
+    
+    //I've to spplit all the calculation in several steps. If i try to do it in a single line the arduino will explode.
+    diflat=radians(flat2-flat1);
+    flat1=radians(flat1);
+    flat2=radians(flat2);
+    diflon=radians((flon2)-(flon1));
+    
+    dist_calc = (sin(diflat/2.0)*sin(diflat/2.0));
+    dist_calc2= cos(flat1);
+    dist_calc2*=cos(flat2);
+    dist_calc2*=sin(diflon/2.0);
+    dist_calc2*=sin(diflon/2.0);
+    dist_calc +=dist_calc2;
+    
+    // This represents the 
+    dist_calc=(2*atan2(sqrt(dist_calc),sqrt(1.0-dist_calc)));
+    
+    dist_calc*=6371000.0; //Converting to meters
+    //Serial.println(dist_calc);
+    return dist_calc;
+}
